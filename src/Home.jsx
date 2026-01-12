3d4a0c94ac1d9941d357fcaf3bebf9cbb5b3e267
JacobWoodbury <you@example.comjacobswoodbury@gmail.comgit config --global user.email jacobswoodbury@gmail.com>
2026-01-12 15:42:58 -0800

    adding on click for fetching plants

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 export default function Home() {
+    const [plants, setPlants] = useState([]);
+     
+    const getPlants = () => {
+        fetch('http://localhost:3306/api/plants')
+            .then(response => response.json())
+            .then(data => setPlants(data));
+    };
     return (
         <div>
             <h1>Home</h1>
-            <button>Request plant list</button>
+            <button onClick={getPlants}>Request plant list</button>
+            {plants.length > 0 && (
+                <ul>
+                    {plants.map(plant => (
+                        <li key={plant.id}>Name: {plant.name} Scientific Name: {plant.scientific_name}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
