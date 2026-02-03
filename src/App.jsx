c6a4a95e95229f03128b61b1978de1eb2a3521e9
JesseChum <jessechum@icloud.com>
2026-02-02 19:51:49 -0800

    Added logic into the empty app.jsx and set it up to take css class names. Also added in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import './App.css'
-import Home from './Home'
+import Home from "./Home"
 
 function App() {
-
+  const [plants] = useState([])
   return (
-    <>
-      <Home />
-    </>
+    <div>
+    <Home />
+
+      {plants.length > 0 && (
+      <div className="plant-grid">
+      {plants.slice(0, 25).map((plant) => (
+    <div key={plant.id} className="plant-card">
+      {plant.name}
+    </div>
+  ))}
+</div>
+  )}
+  </div>
   )
 }
 
 export default App
+
+
