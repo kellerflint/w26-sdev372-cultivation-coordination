ced8bd49beadd8bee551a8f921efba964a4ee092
Jacob Woodbury <jacobswoodbury@gmail.com>
2026-02-19 14:27:36 -0800

    search comments and description

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,3 +6,6 @@ export default function Search() {
         </div>
     )
 }
+//Search database for plants that match the search query
+//if no plants are found call Api search function to get plants from Perenual API
+//we get info that matches (if from api add to db) and then display the plants in the search results
