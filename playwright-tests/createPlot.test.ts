22e5a93f559443eba14d1541a5a04c243d37dae3
Jacob Woodbury <jacobswoodbury@gmail.com>
2026-03-19 12:52:10 -0700

    updating createplots

--- playwright-tests/createPlot.test.ts
+++ playwright-tests/createPlot.test.ts
@@ -4,13 +4,14 @@ test('user can create a plot', async ({ page }) => {
 
   await page.goto('/');
 
+  await page.locator('.toolbarPrimary').click();
   await page.getByPlaceholder('Plot Name').fill('Playwright Plot');
   await page.getByPlaceholder('Plot Description').fill('Playwright Test');
 
   await page.getByPlaceholder('Length (ft)').fill('2');
   await page.getByPlaceholder('Width (ft)').fill('2');
 
-  await page.getByRole('button', { name: 'Add Plot' }).click();
+  await page.locator('.submission').click();
 
   await expect(page.getByText('Playwright Plot')).toBeVisible();
   await expect(page.getByText('Playwright Test')).toBeVisible();
